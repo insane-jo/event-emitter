ca512ce05ed5fcf8f70ac06ef0ec9e2cbae197ff
Anton Strömkvist <anton@stromkvist.com>
2018-03-06 10:41:35 +0100

    Add TypeScript declaration file

---
+++ index.d.ts
@@ -0,0 +1,25 @@
+// Type definitions for event-emitter-es6 1.1
+// Project: https://github.com/insane-jo/event-emitter#readme
+// Definitions by: Anton Strömkvist <https://github.com/ahstro>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare module "event-emitter-es6" {
+    interface Options {
+        emitDelay?: number;
+        strictMode?: boolean;
+    }
+
+    type Listener = (...args: any[]) => void;
+
+    class EventEmitter {
+        constructor(options?: Options);
+        on(type: string, listener: Listener): void;
+        once(type: string, listener: Listener): void;
+        off(type: string, listener?: Listener): void;
+        emit(type: string, ...eventArgs: any[]): void;
+        emitSync(type: string, ...eventArgs: any[]): void;
+        destroy(): void;
+    }
+
+    export = EventEmitter;
+}
